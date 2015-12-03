97c790f87110c74d5a05e972345451a33f55dd1f
Jennifer Yien <jenniferllwu@gmail.com>
2015-12-03 18:17:07 -0500

    angular 2

--- app.js
+++ app.js
@@ -1,31 +1,45 @@
 (function(){
-  angular.module('chuckJokes', [])
-    .controller('Ctrl', function($scope, $http) {
-      $scope.fetch = function() {
-        if ($scope.string == true){
-          $http({
-            method: "GET",
-            url: "http://api.icndb.com/jokes/random?exclude=[explicit]"
-            }).then(function successCallback(response){
-              console.log(response.data.value)
-              $scope.search = response.data.value
-              },
-              function errorCallback(response){
-              console.log("error " + response)
-            })
-          } else {
-          $http({
-            method: "GET",
-            url: "http://api.icndb.com/jokes/random"
-          }).then(function successCallback(response){
-            console.log(response.data.value)
-            $scope.search = response.data.value
-            },
-            function errorCallback(response){
-            console.log("error " + response)
-          })
-        }
-      };
+  function AppComponent() {
+    this.search = ''
+    this.explicit = false
+    this.fetch = function() {
+      console.log(this.explicit)
+      if(this.explicit == true) {
+        fetch('http://api.icndb.com/jokes/random?exclude=[explicit]').then(function(response) {
+          return response.json()
+        }).then(function(json) {
+          document.getElementById('result').innerHTML = json.value.joke
+        }).catch(function(ex) {
+          console.log('parsing failed', ex)
+        })
+      } else {
+        fetch('http://api.icndb.com/jokes/random').then(function(response) {
+          return response.json()
+        }).then(function(json) {
+          // console.log('parsed json', json.value.joke)
+          document.getElementById('result').innerHTML = json.value.joke
+        }).catch(function(ex) {
+          console.log('parsing failed', ex)
+        })
+      }
+
+    }
+  }
+
+  AppComponent.annotations = [
+    new angular.ComponentAnnotation({
+      selector: 'titleHead'
+    }),
+    new angular.ViewAnnotation({
+      template: '<h1>Chuck Norris Joke Bank</h1>' +
+      '<button (click)="fetch()">Roundhouse joke me!</button>' + '<br/>' +
+      '<input #string type="checkbox" (change)="explicit = true">No explcit jokes</input>' + '<br/>' +
+      '<div id="joke"><p id="result">{{search}}<p></div>'
+    })
+  ];
+
+  document.addEventListener('DOMContentLoaded', function() {
+    angular.bootstrap(AppComponent);
   });
 
 })();
